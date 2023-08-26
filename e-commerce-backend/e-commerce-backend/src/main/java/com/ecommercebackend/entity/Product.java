7d8274dc25a70de69197a8cb695d70f87b4b0110
lion772 <williamlopes64@gmail.com>
2023-08-26 22:31:41 +0200

    define Product entity fields and map it to db via @Column

--- e-commerce-backend/e-commerce-backend/src/main/java/com/ecommercebackend/entity/Product.java
+++ e-commerce-backend/e-commerce-backend/src/main/java/com/ecommercebackend/entity/Product.java
@@ -3,17 +3,45 @@ package com.ecommercebackend.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Entity
 @Table(name = "product")
-@Data
+@Data // lombok annotation will automatically generate getter/setter methods behind the scenes.
 public class Product {
-
-
   //define fields
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id")
   private Long id;
+
+  @Column(name = "sku")
+  private String sku;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "unit_price")
+  private BigDecimal unitPrice;
+
+  @Column(name = "image_url")
+  private String imageUrl;
+
+  @Column(name = "active")
+  private boolean active;
+  
+  @Column(name = "units_in_stock")
+  private int unitsInStock;
+
+  @Column(name = "date_created")
+  private Date dateCreated;
+
+  @Column(name = "last_updated")
+  private Date lastUpdated;
 
 
   //define constructors
