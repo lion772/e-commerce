8ca3431ed92fa50d9cc577a344c5781265d4aea1
lion772 <williamlopes64@gmail.com>
2023-08-27 08:41:42 +0200

    generate ProductRepository and ProdutctCategoryRepository, extend to JpaRepository and pass their corresponsing entity type for performing common database operations

---
+++ e-commerce-backend/e-commerce-backend/src/main/java/com/ecommercebackend/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommercebackend.dao;
+
+import com.ecommercebackend.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+// 'collectionResourceRel' is the name of the actual JSON entry
+//path: '/product-category', instead of adding 's' in the end.
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
