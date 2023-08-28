c421c0d796196ebdeb853dc0e3517ea25028e67a
lion772 <williamlopes64@gmail.com>
2023-08-28 13:24:28 +0200

    generate Product class, inject ProductService into ProductList component, call retreieveProducts to return an Observable of Product[], render it on the template, write tests to Product class and ProductList using jasmine

---
+++ src/app/common/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+  //define construct - it declares and assigns properties automatically
+  public constructor(
+    public sku: string,
+    public name: string,
+    public description: string,
+    public unitPrice: number,
+    public imageUrl: string,
+    public active: boolean,
+    public unitsInStock: number,
+    public dateCreated: Date,
+    public lastUpdated: Date
+  ) {}
+}
--- src/app/components/product-list/product-list.component.spec.ts
+++ src/app/components/product-list/product-list.component.spec.ts
@@ -1,21 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Observable, of } from 'rxjs';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 import { ProductListComponent } from './product-list.component';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  let mockProductService: jasmine.SpyObj<ProductService>;
 
   beforeEach(() => {
+    mockProductService = jasmine.createSpyObj('ProductService', [
+      'retrieveProductList',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [ProductListComponent]
+      declarations: [ProductListComponent],
+      providers: [{ provide: ProductService, useValue: mockProductService }],
     });
+
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should retrieve product list from ProductService', () => {
+    const mockData: Product[] = [
+      {
+        sku: 'BOOK-TECH-1000',
+
+        name: 'JavaScript - The Fun Parts',
+
+        description: 'Learn JavaScript',
+
+        unitPrice: 19.99,
+
+        imageUrl: 'assets/images/products/placeholder.png',
+
+        active: true,
+
+        unitsInStock: 100,
+
+        dateCreated: new Date('2023-08-25'),
+
+        lastUpdated: new Date('2023-08-27'),
+      },
+    ];
+    const mockProducts: Product[] = mockData;
+    mockProductService.retrieveProductList.and.returnValue(of(mockProducts));
+
+    fixture.detectChanges(); // Trigger ngOnInit
+
+    expect(component.productList$).toBeDefined();
+    component.productList$.subscribe((products) => {
+      expect(products).toEqual(mockProducts);
+    });
+
+    //should retrieve product list on initialization
+    expect(mockProductService.retrieveProductList).toHaveBeenCalled();
+    expect(component.productList$).toBeInstanceOf(Observable);
   });
 });
