54d6024c18be174c775c822fa82e960ac17f2aff
lion772 <williamlopes64@gmail.com>
2023-09-23 17:15:08 +0200

    adapt frontend to display products by id, expose product category id on backend

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-category-menu',
   templateUrl: './product-category-menu.component.html',
-  styleUrls: ['./product-category-menu.component.css']
+  styleUrls: ['./product-category-menu.component.css'],
 })
-export class ProductCategoryMenuComponent {
+export class ProductCategoryMenuComponent implements OnInit {
+  public productCategories$!: Observable<ProductCategory[]>;
 
+  public constructor(public productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productCategories$ = this.productService.getProductCategories();
+  }
 }
