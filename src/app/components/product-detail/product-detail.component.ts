f62f1be42642595cb626a8cbe2152aa945f82bea
lion772 <williamlopes64@gmail.com>
2023-10-09 12:52:05 +0200

    add new components

---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit{
+  public selectedProduct$!: Observable< Product | null>;
+  public isLoading: boolean = true;
+  public productNotFound: boolean = false;
+  public constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const productId = params['id'];
+      this.selectedProduct$ = this.productService.getProductById(productId) as Observable<Product | null>;
+      this.selectedProduct$.subscribe(
+        (product) => {
+          if (product === null) {
+            this.productNotFound = true; // Set the flag to true when no product is found
+          }
+          this.isLoading = false;
+        },
+        (error) => {
+          this.productNotFound = true; // Set the flag to true on error
+          this.isLoading = false;
+        }
+      );
+    });
+
+  }
+
+}
